c62f5e2a38e25e0b2d8e82334274ad0efe045ae2
tonyyasi <tonyyasi@gmail.com>
2017-02-15 17:23:45 -0600

    Initializes project and upates to recommended settings

---
+++ Example/Pods/Target Support Files/Pods-Pomegranate_Example/Pods-Pomegranate_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pomegranate_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Pomegranate_Example
+@end
---
+++ Example/Pods/Target Support Files/Pomegranate/Pomegranate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pomegranate : NSObject
+@end
+@implementation PodsDummy_Pomegranate
+@end
