2e1e548c2d0385eb7746e0808d6b34a099293258
tonyyasi <tonyyasi@gmail.com>
2017-02-20 17:26:08 -0600

    Adds a way to push the new controllers

--- Example/Pomegranate/ViewController.swift
+++ Example/Pomegranate/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import Pomegranate
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let titles : [String] = ["Gradienter", "To be done..."]
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: titles[indexPath.row])
+        self.navigationController?.pushViewController(vc!, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = titles[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
