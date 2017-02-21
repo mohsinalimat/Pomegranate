5a2955ad82d7d5b693f2d12aeb854c34a1fd604a
tonyyasi <tonyyasi@gmail.com>
2017-02-21 15:03:10 -0600

    Adds image cache class to check the cache for an image

---
+++ Pomegranate/Classes/ImageCache.swift
@@ -0,0 +1,28 @@
+//
+//  ImageCache.swift
+//  Pods
+//
+//  Created by Tony Toussaint on 2/21/17.
+//
+//
+
+import Foundation
+import UIKit
+
+public class ImageCache{
+     static let sharedImageCache = ImageCache()
+    let cache = NSCache<NSURL, AnyObject>()
+    
+    func checkCacheForImage(url: NSURL)->UIImage?{
+        if let cachedVersion = cache.object(forKey: url) as? UIImage {
+            return cachedVersion
+        }
+        else {
+            return nil
+        }
+    }
+    func addImageToCache(url: NSURL, imageToSave: UIImage){
+        
+        cache.setObject(imageToSave, forKey: url)
+    }
+}
---
+++ Pomegranate/Classes/UIImageView+Downloaderr.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageView+Downloaderr.swift
+//  Pods
+//
+//  Created by Tony Toussaint on 2/21/17.
+//
+//
+
+import Foundation
