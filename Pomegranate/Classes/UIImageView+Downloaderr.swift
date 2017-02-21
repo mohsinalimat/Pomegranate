0dbe95589c4e8bbc2fa18be2a33a156bb224c61e
tonyyasi <tonyyasi@gmail.com>
2017-02-21 15:03:38 -0600

    Adds the UIView extension to download an image on the background given an URL

--- Pomegranate/Classes/UIImageView+Downloaderr.swift
+++ Pomegranate/Classes/UIImageView+Downloaderr.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+
+public extension UIImageView{
+    public func downloadImageWithPlaceholder(url: NSURL, placeholder: UIImage){
+    
+        
+        let response = ImageCache.sharedImageCache.checkCacheForImage(url: url)
+        if (response != nil ) {
+            self.image = response
+            
+        }
+        else {
+            self.image = placeholder
+            let downloadQueue = DispatchQueue(label: "download")
+            
+            downloadQueue.async {
+            
+                let data = NSData(contentsOf: url as URL)
+                var imageToSet = UIImage()
+                
+                if(data != nil) {
+                    imageToSet = UIImage(data: data! as Data)!
+                }
+                
+                DispatchQueue.main.async(execute: {
+                    self.image = imageToSet
+                    ImageCache.sharedImageCache.addImageToCache(url: url, imageToSave: imageToSet)
+                })
+            }
+        }
+
+    }
+}
