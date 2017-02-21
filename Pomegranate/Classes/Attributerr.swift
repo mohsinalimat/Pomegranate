80294882e181147b056c10189882985a3afe5d8a
tonyyasi <tonyyasi@gmail.com>
2017-02-21 16:56:26 -0600

    Adds the attributer string

---
+++ Pomegranate/Classes/Attributerr.swift
@@ -0,0 +1,16 @@
+//
+//  Attributerr.swift
+//  Pods
+//
+//  Created by Tony Toussaint on 2/21/17.
+//
+//
+
+import Foundation
+import UIKit
+
+public class Attributerr{
+    public static sharedInstance = Attributerr()
+    
+    
+}
