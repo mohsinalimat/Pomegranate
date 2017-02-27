679c749505887a1e25af4f2233dd8c99ecbcefd8
tonyyasi <tonyyasi@gmail.com>
2017-02-27 16:17:59 -0600

    Adds Attributerr to the tableView

--- Example/Pomegranate/ViewController.swift
+++ Example/Pomegranate/ViewController.swift
@@ -11,7 +11,7 @@ import Pomegranate
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let titles : [String] = ["Gradienter","Downloaderr","DateFormatterr"]
+    let titles : [String] = ["Gradienter","Downloaderr","DateFormatterr","Attributerr"]
     
     @IBOutlet weak var tableView: UITableView!
 
