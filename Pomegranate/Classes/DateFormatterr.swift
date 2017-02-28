3dcfd06a62d539dba4b1d28787bbb9bc332285a5
Jelizondo <jelizondolevy.s@gmail.com>
2017-02-28 11:34:34 -0600

    Refactors Date extension

--- Pomegranate/Classes/DateFormatterr.swift
+++ Pomegranate/Classes/DateFormatterr.swift
@@ -8,21 +8,21 @@
 
 import Foundation
 
-extension Date {
+public extension Date {
     
     //Time in seconds
-    public func minutesInSeconds() -> Int {return 60}
-    public func hourInSeconds() -> Int {return 3600}
-    public func dayInSeconds() -> Int {return 864000}
-    public func yearInSeconds() -> Int {return 31536000}
+    var minutesInSeconds:Double { return 60 }
+    var hourInSeconds:Double { return 3600 }
+    var dayInSeconds:Double { return 86400 }
+    var yearInSeconds:Double { return 31536000 }
     
     //Time intervals
     func timeIntervalInDaysSinceNow(date:Date) -> Double {
-        return date.timeIntervalSinceNow / Double(dayInSeconds())
+        return date.timeIntervalSinceNow / dayInSeconds
     }
     
     func timeIntervalInYearsSinceNow(date:Date) -> Double {
-        return date.timeIntervalSinceNow / Double(yearInSeconds())
+        return date.timeIntervalSinceNow / yearInSeconds
     }
     
     //Converting strings to dates
@@ -34,24 +34,28 @@ extension Date {
     }
     
     //Converting dates to strings
-    public func timeAgo(date:Date) -> String {
-        let timeIntervalInDays = Int(date.timeIntervalSinceNow/(60*60*24) * -1)
-        let timeIntervalInYears = Int(date.timeIntervalSinceNow/(60*60*24*365) * -1)
+    func timeAgo(date:Date) -> String {
+        let timeIntervalInDays = Int(date.timeIntervalInDaysSinceNow(date: date) * -1)
+        let timeIntervalInYears = Int(date.timeIntervalInYearsSinceNow(date: date) * -1)
         
-        if timeIntervalInYears >= 1 {
-            if timeIntervalInYears == 1 {
-                return "1 year and \(timeIntervalInDays-(365*timeIntervalInYears)) days ago"
-            }
+        if timeIntervalInYears == 1 {
+            return "1 year and \(timeIntervalInDays-(365*timeIntervalInYears)) days ago"
+        }
+
+        if timeIntervalInYears > 1 {
             return "\(timeIntervalInYears) years and \(timeIntervalInDays-(365*timeIntervalInYears)) days ago"
         }
-        else if timeIntervalInDays >= 1 {
-            if timeIntervalInDays == 1 {
-                return "1 day ago"
-            }
+        
+        if timeIntervalInDays == 1 {
+            return "1 day ago"
+        }
+        
+        if timeIntervalInDays > 1 {
+            
             return "\(timeIntervalInDays) days ago"
         }
-        else {
-            return "A moment ago"
-        }
+        
+        return "A moment ago"
+        
     }
 }
