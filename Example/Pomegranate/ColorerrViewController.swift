e39fa53d8746424e0f02a8ce5a9da9e03321281c
tonyyasi <tonyyasi@gmail.com>
2017-03-07 15:19:46 -0600

    Sets the background color of the views

--- Example/Pomegranate/ColorerrViewController.swift
+++ Example/Pomegranate/ColorerrViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Pomegranate
 
 class ColorerrViewController: UIViewController {
+    
+    @IBOutlet weak var view1: UIView!
 
+    @IBOutlet weak var view2: UIView!
+    
+    @IBOutlet weak var view3: UIView!
+    
+    @IBOutlet weak var view4: UIView!
+    
+    @IBOutlet weak var view5: UIView!
+    
+    @IBOutlet weak var view6: UIView!
+    
+    @IBOutlet weak var view7: UIView!
+    
+    @IBOutlet weak var view8: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view1.backgroundColor = UIColor.Colorerr.Blue.light
+        view2.backgroundColor = UIColor.Colorerr.Blue.dark
+        view3.backgroundColor = UIColor.Colorerr.Pink.light
+        view4.backgroundColor = UIColor.Colorerr.Pink.dark
+        view5.backgroundColor = UIColor.Colorerr.Orange.regular
+        view6.backgroundColor = UIColor.Colorerr.Orange.dark
+        view7.backgroundColor = UIColor.Colorerr.SocialMedia.twitter
+        view8.backgroundColor = UIColor.Colorerr.SocialMedia.facebook
+        
+        
         // Do any additional setup after loading the view.
     }
 
