2e455c6aeeeb68c88218662908542e19cc8de756
tonyyasi <tonyyasi@gmail.com>
2017-03-13 16:43:26 -0600

    Adds functionality to showcase multipleColorGradients

--- Example/Pomegranate/GradientViewController.swift
+++ Example/Pomegranate/GradientViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Pomegranate
 
 class GradientViewController: UIViewController {
+    @IBOutlet weak var multipleColorSwitch: UISwitch!
 
+    let colors = [UIColor.Colorerr.Blue.regular.cgColor, UIColor.Colorerr.Green.regular.cgColor, UIColor.Colorerr.Yellow.regular.cgColor]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,11 +24,16 @@ class GradientViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func leftButtonPressed(_ sender: UIButton) {
         if(!sender.isSelected){
             sender.isSelected = true
+            if(multipleColorSwitch.isOn){
+                view.addGradientWithMultipleColors(colors: colors, type: .leftToRight, animated: true)
+            }
+            else{
             view.addGradient(beginingColor: .blue, endingColor: .yellow, type: .leftToRight, animated: true)
+            }
         }
         else{
             sender.isSelected = false
@@ -35,7 +44,12 @@ class GradientViewController: UIViewController {
     @IBAction func topButtonPressed(_ sender: UIButton) {
         if(!sender.isSelected){
             sender.isSelected = true
+            if(multipleColorSwitch.isOn){
+                view.addGradientWithMultipleColors(colors: colors, type: .topToBottom, animated: true)
+            }
+            else{
             view.addGradient(beginingColor: .blue, endingColor: .yellow, type: .topToBottom, animated: true)
+            }
         }
         else{
             sender.isSelected = false
@@ -46,7 +60,12 @@ class GradientViewController: UIViewController {
     @IBAction func rightButtonPressed(_ sender: UIButton) {
         if(!sender.isSelected){
             sender.isSelected = true
+            if(multipleColorSwitch.isOn){
+                view.addGradientWithMultipleColors(colors: colors, type: .rightToLeft, animated: true)
+            }
+            else{
             view.addGradient(beginingColor: .blue, endingColor: .yellow, type: .rightToLeft, animated: true)
+            }
         }
         else{
             sender.isSelected = false
@@ -57,7 +76,12 @@ class GradientViewController: UIViewController {
     @IBAction func bottomButtonPressed(_ sender: UIButton) {
         if(!sender.isSelected){
             sender.isSelected = true
+            if(multipleColorSwitch.isOn){
+             view.addGradientWithMultipleColors(colors: colors, type: .bottomToTop, animated: true)
+            }
+            else{
             view.addGradient(beginingColor: .blue, endingColor: .yellow, type: .bottomToTop, animated: true)
+            }
         }
         else{
             sender.isSelected = false
@@ -68,7 +92,12 @@ class GradientViewController: UIViewController {
     @IBAction func diagonal1ButtonPressed(_ sender: UIButton) {
         if(!sender.isSelected){
             sender.isSelected = true
+            if(multipleColorSwitch.isOn){
+                view.addGradientWithMultipleColors(colors: colors, type: .diagonalLeftToRight, animated: true)
+            }
+            else{
             view.addGradient(beginingColor: .blue, endingColor: .yellow, type: .diagonalLeftToRight, animated: true)
+            }
         }
         else{
             sender.isSelected = false
@@ -79,7 +108,12 @@ class GradientViewController: UIViewController {
     @IBAction func diagonal2ButtonPressed(_ sender: UIButton) {
         if(!sender.isSelected){
             sender.isSelected = true
+            if(multipleColorSwitch.isOn){
+                view.addGradientWithMultipleColors(colors: colors, type: .diagonalRightToLeft, animated: true)
+            }
+            else{
             view.addGradient(beginingColor: .blue, endingColor: .yellow, type: .diagonalRightToLeft, animated: true)
+            }
         }
         else{
             sender.isSelected = false
